6cd786e9dff3a31ded4fb032647864c9a28c40d4
Ulbert <e.k.porietis@student.utwente.nl>
2025-06-19 01:49:16 +0200

    testing deplyment

--- API/DB/db.js
+++ API/DB/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 require('dotenv').config();
-
+console.log(process.env.DB_HOST, process.env.DB_USER, process.env.DB_PORT, process.env.DB_PASSWORD, process.env.DB_NAME)
 // Create a connection pool
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
